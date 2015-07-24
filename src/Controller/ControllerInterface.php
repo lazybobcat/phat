2ae60f216c166a2bf3905e7fc74c6030aaa0abd6
Lo-X <loic.boutter@gmail.com>
2015-07-25 00:41:01 +0200

    Adding Dispatcher and Controller classes

---
+++ src/Controller/ControllerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Phat\Controller;
+
+
+interface ControllerInterface {
+    public function beforeAction();
+    public function afterAction();
+    public function beforeRender();
+    public function afterRender();
+}
