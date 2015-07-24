ae9ee4fa669721374174f270267c8585db78f769
Lo-X <loic.boutter@gmail.com>
2015-07-25 00:56:23 +0200

    TODOs

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -7,6 +7,11 @@ use Phat\Http\Request;
 
 class Controller implements ControllerInterface {
 
+    // TODO : View class and sending vars to view, easy render, etc.
+    // TODO : Helpers and Components
+    // TODO : easy 404
+    // TODO : easy redirect
+
     protected   $request;
 
     public function __construct(Request $request)
