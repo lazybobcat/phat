bf48b191ffef0091dfac8d505353c6076bc3dab4
Lo-X <loic.boutter@gmail.com>
2015-07-30 20:39:24 +0200

    Test suite and first tests

---
+++ tests/CommonTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Phat\Test;
+
+use Phat\Core\Configure;
+use Phat\TestTool\TestCase;
+
+class CommonTest extends TestCase
+{
+
+    public function testDebug()
+    {
+        $expected = <<< EOT
+<pre>
+
+> \$object - %s:%d
+=========================================================
+string(12) "Hello World!"
+</pre>
+EOT;
+
+        $expected = sprintf($expected, __FILE__, __LINE__ + 2);
+        $this->expectOutputString($expected);
+        debug("Hello World!", false); // Text mode
+    }
+
+    public function testDev()
+    {
+        Configure::write('debug', true);
+        $this->assertEquals(true, Configure::read('debug'));
+
+        Configure::write('debug', false);
+        $this->assertEquals(false, Configure::read('debug'));
+    }
+
+    public function testNamespaceSplit()
+    {
+        $result = namespaceSplit("Hey");
+        $this->assertEquals(['Hey'], $result);
+
+        $result = namespaceSplit("\Hey");
+        $this->assertEquals(['Hey'], $result);
+
+        $result = namespaceSplit("Hey\Ho");
+        $this->assertEquals(['Hey', 'Ho'], $result);
+
+        $result = namespaceSplit("\Hey\Ho\You");
+        $this->assertEquals(['Hey', 'Ho', 'You'], $result);
+    }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once 'vendor/autoload.php';
+require_once 'src/common.php';
+
+use Phat\Core\Configure;
+
+$config = [
+    'debug' =>  true,
+    'App'   => [
+        // The base url, ie: 'http://example.com' without the final '/'
+        'baseUrl'   => '',
+        'dir'       => 'src',
+        'webrootDir'=> 'tests/test_app/webroot',
+        'appDir'    => 'tests/test_app/app',
+        'pluginsDir'=> 'tests/test_app/plugins',
+        // The view dir will be situated under appDir/ folder or a specific pluginsDir/<plugin>/ folder
+        'viewDir'   => 'Template',
+        'encoding'  => 'utf-8'
+    ]
+];
+
+Configure::write($config);
